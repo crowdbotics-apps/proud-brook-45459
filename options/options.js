7a204a87d0b6a6f83364cad08670c5024caf2fad
Crowdbotics <team@crowdbotics.com>
2024-01-20 21:07:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-brook-45459",
+  url: "https://proud-brook-45459.botics.co",
+  api: "https://proud-brook-45459.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
